3829e1cb2a6d22f5cc23c32271ad2a09dbca59a3
marcantonio99 <marcantonio8799@gmail.com>
2023-08-08 17:36:59 +0200

    search

--- src/main/java/org/project/springlibrary/controller/BookController.java
+++ src/main/java/org/project/springlibrary/controller/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -22,11 +23,19 @@ public class BookController {
     private BookRepository bookRepository;
 
     @GetMapping
-    public String list(Model model) {
-        //recupero la lista dei libri dal database
-        List<Book> books = bookRepository.findAll();
+    public String list(@RequestParam(name = "keyword", required = false) String searchString, Model model) {
+        List<Book> books;
+        if (searchString == null || searchString.isBlank()) {
+            //recupero la lista dei libri dal database
+            books = bookRepository.findAll();
+        } else {
+            //se ho il parametro searchString faccio la query con filtro
+            //books = bookRepository.findByTitle(searchString);
+            books = bookRepository.findByTitleContainingIgnoreCaseOrAuthorsContainingIgnoreCase(searchString, searchString);
+        }
         //passo la lista dei libri alla view
         model.addAttribute("bookList", books);
+        model.addAttribute("searchInput", searchString == null ? "" : searchString);
         //restituisco il nome template della view
         return "/books/list";
     }
--- src/main/java/org/project/springlibrary/repository/BookRepository.java
+++ src/main/java/org/project/springlibrary/repository/BookRepository.java
@@ -3,6 +3,12 @@ package org.project.springlibrary.repository;
 import org.project.springlibrary.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Integer> {
+import java.util.List;
 
+public interface BookRepository extends JpaRepository<Book, Integer> {
+    //metodo per cercare i libri con titolo uguale a quello che gli viene passato
+    List<Book> findByTitle(String title);
+
+    //metodo per cercare i libri il cui titolo o autore contiene una stringa passata
+    List<Book> findByTitleContainingIgnoreCaseOrAuthorsContainingIgnoreCase(String title, String authors);
 }
